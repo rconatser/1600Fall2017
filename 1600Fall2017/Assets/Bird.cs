ebfc3f5d097df7498c99b713bd22bdd11cc5ded7
Unknown <10806252@my.uvu.edu>
2017-09-01 14:37:27 -0600

    Added what child classes do comment

--- 1600Fall2017/Assets/Bird.cs
+++ 1600Fall2017/Assets/Bird.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Chil
+
 public class Bird : Animal {
 
-    public override void Start() {
-        base.Start();
+// override start -> overrides what the parent class  says to do and replaces it with it's own function(s)
+    public override void Start() { 
+        base.Start(); // reintroduces original parent class behavior
         Fly();
     }
 
