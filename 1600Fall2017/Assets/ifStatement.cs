a4e59595847ad53b551cf34a849f52423f3ca2ea
Rachel Conatser <10806252@my.uvu.edu>
2017-09-11 11:33:38 -0600

    Playing with if statements, made ifStatements class

---
+++ 1600Fall2017/Assets/ifStatement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifStatement : MonoBehaviour {
+
+	public int num1;
+	public int num2;
+	public int value;
+	public bool canPlay = true;
+
+
+	// Use this for initialization
+	void Start () {
+		if(num1 + num2 == value){
+			print(value);
+		}
+
+		if(canPlay) {
+			print("Play");
+		}
+	}
+
+}
