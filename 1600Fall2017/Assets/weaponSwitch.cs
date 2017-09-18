6e9a765a91f59d6ed905f5a4d506c0eb1d5c262e
Rachel Conatser <10806252@my.uvu.edu>
2017-09-18 11:41:49 -0600

    Added weaponSwitch, worked with arrays

---
+++ 1600Fall2017/Assets/weaponSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponSwitch : MonoBehaviour {
+
+	public string[] weapons; // [] makes it an array
+	public int i = 0; // short for indexer
+
+	public void Start(){
+		switch(weapons[i]){
+			case "Missile": 
+				print("Missile");
+				break;
+
+			case "Plasma":
+				print("Plasma");
+				break;
+
+			default:
+				print("You have no other weapons!");
+				break;
+
+		}
+	}
+
+
+}
