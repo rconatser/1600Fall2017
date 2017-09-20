d45694cc4f5cf90adaa2c24f4ce8f5aae98a5515
Rachel Conatser <10806252@my.uvu.edu>
2017-09-20 11:44:41 -0600

    moved scripts into folder, made while loop

---
+++ 1600Fall2017/Assets/loops.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loops : MonoBehaviour {
+
+	public bool canPlay = true;
+
+	void Start(){
+		StartCoroutine(playGame());
+	}
+
+	IEnumerator playGame(){
+		while (canPlay){
+			print("Playing");
+			yield return new WaitForSeconds(0.1f);
+		}
+		
+		print("End game");
+	}
+
+}
