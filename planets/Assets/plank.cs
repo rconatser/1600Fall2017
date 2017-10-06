e8144cee067c7abe97e544183ea67b6a1a961176
Rachel Conatser <10806252@my.uvu.edu>
2017-10-06 13:11:16 -0600

    worked with Void Functions, made test game and plank movement script

---
+++ planets/Assets/plank.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plank : voidFunctions {
+
+	public float damage = 5f;
+	public float speed = 200f;
+
+	void Update () {
+		transform.Rotate(Vector3.right * Time.deltaTime * speed);
+	}
+}
