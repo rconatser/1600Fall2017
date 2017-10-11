2cca08cb051819882dcf1ad94b1d3c4056e03877
Unknown <10806252@my.uvu.edu>
2017-10-11 11:51:28 -0600

    added enemy script and nav mesh stuff

---
+++ 1600Fall2017/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public NavMeshAgent agent; // makes AI move, part of AI namespace
+	public Transform player; // contain position data, named player
+
+	
+	// Update is called once per frame
+	void Update () {
+		agent.destination = player.position; // vector3(XYZ) assigns value of player's position
+	}
+
+	public void stun (float _speed){
+		agent.speed = _speed;
+		agent.acceleration = _speed;
+		agent.destination = transform.position;
+	}
+}
