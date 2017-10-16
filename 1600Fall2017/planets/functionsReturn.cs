f9ae92f873aaddff6ccc073247bce3eab5c585d2
Rachel Conatser <10806252@my.uvu.edu>
2017-10-16 11:13:27 -0600

    working on functions with returns...

--- 1600Fall2017/planets/functionsReturn.cs
+++ 1600Fall2017/planets/functionsReturn.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class functionsReturn : MonoBehaviour {
-
+	public GameObject player;
 	Vector3 coords;
 	string[] position = {"X","Y","Z"};
+
+	public void start(){
+
+		getX();
+
+	}
+
+		int getX(int x = 0){
+			for(int i = 0; i < position.Length; i++){ // prints the position of X
+				
+			}
+		return x;
+	}
 
 }
