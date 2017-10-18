c34d4a5855ca07e15f70907c39b16f59f0a6ea1c
Rachel Conatser <10806252@my.uvu.edu>
2017-10-18 11:45:56 -0600

    played with characterControl and navMeshAI

--- 1600Fall2017/Assets/Scripts/character/characterControl.cs
+++ 1600Fall2017/Assets/Scripts/character/characterControl.cs
@@ -7,20 +7,17 @@ public class characterControl : MonoBehaviour {
 	public CharacterController characterController;
 	public float gravity = 9.81f; // creates a gravity component, defaults to 9.81
 	public float speed = 10;
-	public float jumpForce = 100; // creates jump 
+	public float jumpForce = 50; // creates jump 
 	public Vector3 move; // temp variable that will store temp data
-	void Start () {
-		
-	}
 
-	void FixedUpdate () {
+	void Update () {
 		move.y -= gravity * Time.deltaTime; // will slow this down and make it coorelate with time
 
 		if(characterController.isGrounded){ // looks to see if character is on ground first
 			move.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // if so, can move
 
-			if(Input.GetKey(KeyCode.Space)){ // if its on ground, can jump
-				move.y += jumpForce * Time.deltaTime; // creates jumpforce (shoots up)
+			if(Input.GetKeyDown(KeyCode.Space)){ // if its on ground, can jump
+				move.y = jumpForce * Time.deltaTime; // creates jumpforce (shoots up)
 			}
 		}
 	
