0b6775ba3e4ceed4ce0986b9e9acf6353712ea61
Rachel Conatser <10806252@my.uvu.edu>
2017-10-25 11:42:00 -0600

    updating healthBar

--- 1600Fall2017/Assets/Scripts/updateHealth.cs
+++ 1600Fall2017/Assets/Scripts/updateHealth.cs
@@ -7,18 +7,38 @@ public class updateHealth : MonoBehaviour {
 
 	public Image healthBar;
 	public float barTime = 0.1f;
+	public float powerLevel = 0.1f;
+	public enum powerUpType{ // like an array/list
+		powerUp, // choice 1 label
+		powerDown // choice 2 label
+	}
+
+	public powerUpType powerUp; // will create a drop down in unity
 
 	void OnTriggerEnter(){
-		StartCoroutine(UpdateBar());
+		switch (powerUp)
+		{
+			case powerUpType.powerUp:
+				StartCoroutine(powerUpBar());
+				break;
 
-		if(healthBar.fillAmount == 0){
+			case powerUpType.powerDown:
+				StartCoroutine(powerDownBar());
+				break;
+		}
 
+	}
+
+	IEnumerator powerUpBar(){
+		while(healthBar.fillAmount < 1){
+			healthBar.fillAmount += powerLevel; // takes away the fillAmount from the barTime
+			yield return new WaitForSeconds(barTime); // waits for time (object)
 		}
 	}
 
-	IEnumerator UpdateBar(){
-		while(healthBar.fillAmount < 1){
-			healthBar.fillAmount += barTime; // takes away the fillAmount from the barTime
+	IEnumerator powerDownBar(){
+		while(healthBar.fillAmount > 0){
+			healthBar.fillAmount -= powerLevel; // takes away the fillAmount from the barTime
 			yield return new WaitForSeconds(barTime); // waits for time (object)
 		}
 	}
