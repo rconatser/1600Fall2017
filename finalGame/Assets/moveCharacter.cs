7cf69532d12eedf1f36892ab8bc26e4f9d097831
Unknown <10806252@my.uvu.edu>
2017-12-05 23:07:05 -0700

    Updated map, trying to figure out how to create Teleports

--- finalGame/Assets/moveCharacter.cs
+++ finalGame/Assets/moveCharacter.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class moveCharacter : MonoBehaviour {
 	public CharacterController characterController;
 	public static bool gameOver; // by default bool is false
+	public static bool intro;
 	public float gravity = 9.81f; // creates a gravity component, defaults to 9.81
 	public float speed = 50;
 	public float jumpForce = 125; // creates jump 
 	public Vector3 move; // temp variable that will store temp data
 
+	void Start()
+	{
+		//playerName.getPlayerName(getName.text); // assigns input to string name.
+	}
+
 	void Update () {
 		move.y -= gravity * Time.deltaTime; // will slow this down and make it coorelate with time
 
-		if(characterController.isGrounded && !gameOver){ // looks to see if character is on ground first
+		if(characterController.isGrounded && !gameOver && !intro){ // looks to see if character is on ground first
 			move.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // if so, can move
 
 			if(Input.GetKey(KeyCode.Space)){ // if its on ground, can jump
 				move.y = jumpForce * Time.deltaTime; // creates jumpforce (shoots up)
 			}
 		}
-	
 		characterController.Move(move);
 
 	}
--- finalGame/Assets/playerName.cs
+++ finalGame/Assets/playerName.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerName : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public InputField getName; // creates a field where the player can type an input.
+	public GameObject introUI;
+
+	public void Start(){
+		getPlayerName(getName.text);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public string getPlayerName (string name){
+		introUI.SetActive(true);
+		moveCharacter.intro = true;
+
+		if(name != ""){ // if player name input does not equal 'nothing' will return a name.
+			moveCharacter.intro = false;
+			introUI.SetActive(false);
+
+		}else{
+			name = "Player";
+			moveCharacter.intro = false;
+			introUI.SetActive(false);
+		}
+		return name;
 	}
 }
