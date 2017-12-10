b9c1944593513639e2387d6dbd6906922a9d83b6
Unknown <10806252@my.uvu.edu>
2017-12-10 13:37:48 -0700

    Added colors arrays, added checkpoint to TP Script

---
+++ finalGame/Assets/changeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColor : MonoBehaviour {
+	
+	public Color[] lifeColors = new Color[4]; // creates an array to hold the 4 Colors used for health bar color/lives text
+	public Color[] groundColor = new Color[5]; // creates another array for ground color later in level
+
+	void Start(){
+		lifeColors[0] = new Color(114,255,189,255); // deepish green color
+		lifeColors[1] = new Color(50,255,242,255); // yellow color
+		lifeColors[2] = new Color(34,255,236,255); // orange color
+		lifeColors[3] = new Color(0,255,210,255); // deepish red color
+
+		groundColor[0] = Color.white;
+		groundColor[1] = Color.yellow;
+		groundColor[2] = Color.green;
+		groundColor[3] = Color.cyan;
+		groundColor[4] = Color.blue;
+	}
+
+}
--- finalGame/Assets/teleport.cs
+++ finalGame/Assets/teleport.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class teleport : MonoBehaviour {
-
-	public GameObject teleportOne;
 	public Transform player;
 
 	
 	void OnTriggerEnter(){	
-		if(teleportOne){
-			player.position = new Vector3(-18,224,-55); // teleports player to new coordinates
-		}
+		player.position = new Vector3(-18,224,-55); // teleports player to new coordinates
+		replay.startPosition = player.position; // logs teleported coordinates as new spawn coordinates
   	}
 }
